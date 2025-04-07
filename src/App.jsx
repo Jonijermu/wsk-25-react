282c84d810d50253d7dc13e6a59f8db039dda0ce
joni.heikkila <joni.heikkila@metropolia.fi>
2025-04-07 12:15:24 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Greeting from './Greeting.jsx';
+
+function App() {
+
+  return (
+    <>
+      <h1>My App</h1>
+      <Greeting name="Joni"/>
+    </>
+  );
+}
+
+
+export default App
